53aacbbe0613d4fed265a96e78c62ea6e12989e3
ALFIIA <208081819+MILANA808@users.noreply.github.com>
2026-09-07 02:39:18 +0300

    AKSI purge v211 — force cache clear on build change

--- aksi-purge.js
+++ aksi-purge.js
@@ -3,7 +3,7 @@
  * © AKSI · aksilove@internet.ru
  */
 (function () {
-  var BUILD = "20260906-v210";
+  var BUILD = "20260907-v211";
   var KEY = "aksi_build_id";
   var RELOAD = "aksi_purged_" + BUILD;
   function log(m) {
